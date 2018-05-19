9c73ea3e311f74a6ee04372bf1b0e6e40bcf4101
itggot-lukas-persson <lukas.persson@itggot.se>
2018-05-19 17:45:05 +0200

    Dropdown menu, color changes, nested grids.

---
+++ site/public/JS/dropdowndam.js
@@ -0,0 +1,18 @@
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
